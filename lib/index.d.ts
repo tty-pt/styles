d6317f1dcd47eb8da025b80b969879c101f015c7
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2023-02-20 09:38:47 +0000

    Fix declarations

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,12 +2,12 @@ import React from "react";
 import { Theme } from "@material-ui/core";
 import { Cast, MagicBook, CastProps, MagicClassesProps, Magic, CastComponentProps } from "./types";
 
+declare function makeMagicBook(theme: Theme): MagicBook;
 declare function useCast(Context: React.Context<Magic>|null): Cast;
 declare function withCast(Com: React.ComponentType<CastProps>): React.FC<CastComponentProps>;
-declare function withMagicBook<P extends object>(Com: React.ComponentType<P>): React.FC<P&MagicClassesProps>;
 /*
  * for use with withStyles(style)(withMagicClasses(Component)).
  * provides you with the Magic context.
  */
-declare function makeMagicClasses(theme: Theme): MagicBook;
+declare function withMagicClasses<P extends object>(Com: React.ComponentType<P>): React.FC<P&MagicClassesProps>;
 declare const MagicBookContext: React.Context<Magic>;
